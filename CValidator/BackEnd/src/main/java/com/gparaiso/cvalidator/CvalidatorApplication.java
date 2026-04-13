6fc4cdd685b6106694ab58cd12da0d5e79193f37
Gustavo Paraíso <gustavoclfp@gmail.com>
2026-04-13 19:21:17 +0100

    Updates

---
+++ CValidator/BackEnd/src/main/java/com/gparaiso/cvalidator/CvalidatorApplication.java
@@ -0,0 +1,13 @@
+package com.gparaiso.cvalidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvalidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvalidatorApplication.class, args);
+	}
+
+}
